48280f76d2b9bf76186bca7732e5853454d652ba
tyranderson01 <tyleranderson827@gmail.com>
2021-04-28 16:53:06 -0600

    Initial commit

---
+++ main2.py
@@ -0,0 +1,54 @@
+import os
+import xlwings as xw
+import datetime
+
+wb1 = xw.Book(r"C:\Users\tyler.anderson\Documents\Finance\GL Codes List for Schedules.xlsm", update_links=False)
+ws = wb1.sheets['Summary']
+data = ws.range("B5:B46").value
+xw.apps.active.quit()
+
+
+def updateGL():
+    path = r"P:\PACS\Finance\Month End Close"
+    path = r"C:\Users\tyler.anderson"
+    main_folders = os.listdir(path)
+    for folder in main_folders:
+        if "Old" not in folder and "cloud" not in folder and "All -" not in folder:
+            print(folder)
+            newpath = path + "\\" + folder
+            for dirpath, dirnames, filenames in os.walk(newpath):
+                for filename in [f for f in filenames if f.endswith("GL Schedules.xlsm")]:
+                    file = os.path.join(dirpath, filename)
+                    print(file)
+                    modified_time = os.path.getmtime(file)
+                    date_value = datetime.datetime.fromtimestamp(modified_time)
+                    print(f"{date_value:%Y-%m-%d %H:%M:%S}")
+                    month = date_value.month
+                    year = date_value.year
+                    if year > 2020 and month > 3:
+                        wb = xw.Book(file, update_links=False)
+                        sum_sht = wb.sheets('Summary')
+                        for i, code in enumerate(data):
+                            sum_sht.range("B" + str(i+5)).value = code
+                        addin_file = xw.Book(r'C:\Users\tyler.anderson\AppData\Roaming\Microsoft\AddIns\1005-Duplicate Sheet.xlam', update_links=False)
+                        macro = addin_file.macro('DupSheet.updateSummary')
+                        macro()
+                        wb.save()
+                        wb.close()
+                    break
+    xw.apps.active.quit()
+
+
+if __name__ == '__main__':
+    updateGL()
+
+
+    # sum_sht.range('C:C').insert()
+    # sum_sht.range('C5').value = 'Account Name'
+    # sum_sht.range('C5').value = '=IFERROR(XLOOKUP(B5,Lists!A:A,Lists!B:B),"not found")'
+    # sum_sht.range('C5').api.AutoFill(sum_sht.range("C5:C50").api)
+    # sum_sht.range('G:G').insert()
+    # sum_sht.range('G4').value = 'M2M Change'
+    # sum_sht.range('G5').value = '=IF(ISNUMBER(CUBEVALUE("pgsql01 Finance FinanceSecure",$B$1,$H$1,A5,E$1))=TRUE,CUBEVALUE("pgsql01 Finance FinanceSecure",$B$1,$H$1,A5,E$1),0)'
+    # sum_sht.range('G5').api.AutoFill(sum_sht.range("G5:G50").api)
+    # sum_sht.range('G:G').hide()
