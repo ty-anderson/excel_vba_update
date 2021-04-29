552c1546b31d2a388beaddab946c07bf554daf10
tyranderson01 <tyleranderson827@gmail.com>
2021-04-29 11:14:37 -0600

    update

--- main2.py
+++ main2.py
@@ -2,13 +2,12 @@ import os
 import xlwings as xw
 import datetime
 
-wb1 = xw.Book(r"C:\Users\tyler.anderson\Documents\Finance\GL Codes List for Schedules.xlsm", update_links=False)
-ws = wb1.sheets['Summary']
-data = ws.range("B5:B46").value
-xw.apps.active.quit()
-
 
 def updateGL():
+    wb1 = xw.Book(r"C:\Users\tyler.anderson\Documents\Finance\GL Codes List for Schedules.xlsm", update_links=False)
+    ws = wb1.sheets['Summary']
+    data = ws.range("B5:B46").value
+    xw.apps.active.quit()
     path = r"P:\PACS\Finance\Month End Close"
     path = r"C:\Users\tyler.anderson"
     main_folders = os.listdir(path)
@@ -17,11 +16,11 @@ def updateGL():
             print(folder)
             newpath = path + "\\" + folder
             for dirpath, dirnames, filenames in os.walk(newpath):
-                for filename in [f for f in filenames if f.endswith("GL Schedules.xlsm")]:
+                for filename in [f for f in filenames if (("GL Schedules" in f) and f.endswith(".xlsm"))]:
                     file = os.path.join(dirpath, filename)
-                    print(file)
                     modified_time = os.path.getmtime(file)
                     date_value = datetime.datetime.fromtimestamp(modified_time)
+                    print(file)
                     print(f"{date_value:%Y-%m-%d %H:%M:%S}")
                     month = date_value.month
                     year = date_value.year
